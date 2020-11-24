966851de2d72951019c3d9695347174ce2f3daec
liaomao <74762152+liaomao@users.noreply.github.com>
2020-11-24 20:18:52 +0800

    Create 112401.键盘点击.App.java
    这个是键盘的监听事件

---
+++ 112401.键盘点击.App.java
@@ -0,0 +1,49 @@
+package st;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class App {
+    JPanel myPanel = new JPanel();
+    JLabel label_txt = new JLabel();
+    JTextArea textArea_A = new JTextArea();
+    JTextArea textArea_B = new JTextArea();
+
+
+    public App(){
+        textArea_A.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                super.keyPressed(e);
+                if (e.getKeyCode() == KeyEvent.VK_UP ){
+                    textArea_B.append("欢迎来到小茂的鼠标监听！\n");
+                }
+                if (e.getKeyCode() == KeyEvent.VK_DOWN ){
+                    textArea_B.setText("");
+                }
+            }
+        });
+    }
+
+    void go(){
+        textArea_A.setBounds(100,100,300,150);
+        textArea_A.setBackground(Color.cyan);
+        textArea_B.setBackground(Color.yellow);
+        textArea_B.setBounds(100,300,300,150);
+        myPanel.add(textArea_A);
+        myPanel.add(textArea_B);
+        JFrame frame = new JFrame("键盘点击事件");
+        myPanel.setLayout(null);
+        myPanel.add(label_txt);
+        frame.setContentPane(myPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setBounds(100,100,500,720);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new App().go();
+    }
+}
