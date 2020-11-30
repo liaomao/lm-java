38fdb66bdf65631a6f7606a61fbe1e2aaf6cf207
liaomao <74762152+liaomao@users.noreply.github.com>
2020-11-30 15:38:48 +0800

    Update 113001.继承父类.R.java

--- 113001.继承父类.R.java
+++ 113001.继承父类.R.java
@@ -1,5 +1,18 @@
-public class App extends R{
-	public static void main(String[] args) {
-		myR();
-	}
+public  class R{
+	static void myR(){
+		String boy_filePath = "img/boy.gif";
+		String girl_filePath = "img/girl.gif";
+		System.out.println(boy_filePath);
+		System.out.println(girl_filePath);
+		int [][]array2d = {
+				{1,2,3},
+				{4,5,6}
+		};
+		for(int row = 0;row < array2d.length;row++){
+			for (int column = 0; column < array2d[0].length; column++) {
+				System.out.print(array2d[row][column]+" ");
+			}
+			System.out.println();
+		}
+	}	
 }
