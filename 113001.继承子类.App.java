e7d93439c5f5b740211dc3db06b0733144fc9dd0
liaomao <74762152+liaomao@users.noreply.github.com>
2020-11-30 15:04:27 +0800

    Create 113001.继承子类.App.java

---
+++ 113001.继承子类.App.java
@@ -0,0 +1,7 @@
+public class App implements R{
+	public static void main(String[] args) {
+		System.out.println(R.boy_filePath);
+		System.out.println(R.girl_filePath);
+		System.out.println(R.array2d);
+	}
+}
