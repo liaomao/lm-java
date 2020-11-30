1b839739a5d234b6272896fbc6c9fc22799b44a7
liaomao <74762152+liaomao@users.noreply.github.com>
2020-11-30 15:28:38 +0800

    Update 113002.实现接口.App.java

--- 113002.实现接口.App.java
+++ 113002.实现接口.App.java
@@ -2,6 +2,11 @@ public class App implements R{
 	public static void main(String[] args) {
 		System.out.println(R.boy_filePath);
 		System.out.println(R.girl_filePath);
-		System.out.println(R.array2d);
+		for(int row = 0;row < array2d.length;row++){
+			for (int column = 0; column < array2d[0].length; column++) {
+				System.out.print(array2d[row][column]+" ");
+			}
+			System.out.println();
+		}
 	}
 }
