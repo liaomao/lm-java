ef7e5aaac9e4f39f99e2fdb38ac31061e1e718c6
liaomao <74762152+liaomao@users.noreply.github.com>
2020-11-30 15:46:38 +0800

    Update 113001.继承父类.R.java

--- 113001.继承父类.R.java
+++ 113001.继承父类.R.java
@@ -1,18 +1,8 @@
 public  class R{
-	static void myR(){
-		String boy_filePath = "img/boy.gif";
-		String girl_filePath = "img/girl.gif";
-		System.out.println(boy_filePath);
-		System.out.println(girl_filePath);
-		int [][]array2d = {
+		static String boy_filePath = "img/boy.gif";
+		static String girl_filePath = "img/girl.gif";
+		static int [][]array2d = {
 				{1,2,3},
 				{4,5,6}
 		};
-		for(int row = 0;row < array2d.length;row++){
-			for (int column = 0; column < array2d[0].length; column++) {
-				System.out.print(array2d[row][column]+" ");
-			}
-			System.out.println();
-		}
-	}	
 }
